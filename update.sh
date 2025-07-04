6bb7f6e0d2d2ad3194a93dd0eadf278e298a7058
Bill Curran <bcurra3@yahoo.com>
2025-07-03 18:45:55 -0700

    installed and tested zipping data after removal of an app (works!) - currently in test role only converted test plugin task to role in this commit (or possibly previous commit)

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 # BEHIND=`git rev-list ...@{u} --count`
 # git log -$BEHIND ...@{u} --pretty=format:"%h - %an, %ar : %s"
 # ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventories/supernas/inventory nas.yml -b -K $1 $2 $3 $4 $5 $6 $7 $8 $9
+ansible-playbook -i inventories/supernas/inventory nas.yml -b -K $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
 # --tags "taskname"
+# prune docker stuff to save space
+echo y | docker image prune
+echo y | docker volume prune
 
