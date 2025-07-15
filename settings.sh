63f97e0694388b93d656e9f32e868051661b5f66
Bill Curran <bcurra3@yahoo.com>
2025-07-15 13:43:51 -0700

    refactor to use community.docker.docker_container also updated my settings and update scripts to be more useful modified my nas.yml which needs to be UNTRACKED

--- settings.sh
+++ settings.sh
@@ -1,2 +1,8 @@
-# cat inventories/supernas/group_vars/nas.yml |grep 'enabled: true'
+#!/bin/bash
+
+if [[ "$1" == "--enabled" ]]; then
+cat inventories/supernas/group_vars/nas.yml |grep 'enabled: true'
+exit
+fi
+
 nano inventories/supernas/group_vars/nas.yml
--- update.sh
+++ update.sh
@@ -1,9 +1,31 @@
+#!/bin/bash
+
+# prune docker stuff to save space
+if [ "$1" = "--prune" ]; then
+    echo y | docker image prune
+    echo y | docker volume prune
+    exit
+fi
+
+# git info
+if [ "$1" = "--git" ]; then
+    echo not implemented yet
 # BEHIND=`git rev-list ...@{u} --count`
 # git log -$BEHIND ...@{u} --pretty=format:"%h - %an, %ar : %s"
-# ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventories/supernas/inventory nas.yml -b -K $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
-# --tags "taskname"
-# prune docker stuff to save space
-#echo y | docker image prune
-#echo y | docker volume prune
+    exit
+fi
 
+# install ansible-nas requirements
+if [ "$1" = "--requirements" ]; then
+    ansible-galaxy install -r requirements.yml
+    exit
+fi
+
+# update.sh help menu
+if [ "$1" = "--help" ]; then
+    echo "--help menu placeholder"
+    exit
+fi
+
+# --tags "taskname"
+ansible-playbook -i inventories/supernas/inventory nas.yml -b -K $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
