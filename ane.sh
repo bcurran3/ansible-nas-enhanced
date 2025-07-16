cb738c6c1bbf492aedadcad474175ca904c10779
Bill Curran <bcurra3@yahoo.com>
2025-07-15 19:55:04 -0700

    updates to ane.sh

--- ane.sh
+++ ane.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 if [[ "$1" == "--enabled" ]]; then
+     echo "*** ANE enabled apps:"
      cat inventories/supernas/group_vars/nas.yml |grep 'enabled: true'
      exit
 fi
 
-# git info
-if [ "$1" = "--git" ]; then
-    echo "# of commits behind:"
-    git fetch
-    git rev-list --count HEAD..@{u}
-    exit
-fi
-
 # git force pull
+# DO NOT USE: for dev use
 if [ "$1" = "--gitforcepull" ]; then
     git fetch --all
     git reset --hard origin/main
+    exit
+fi
+
+# install ANE
+if [ "$1" = "--install" ]; then
+    echo "Not implemented yet. (Sorry!)"
+# copy default group_vars and inventory stuff here
+    exit
+fi
+
+# git info
+if [ "$1" = "--outdated" ]; then
+#TDL: add --updates
+    echo "# of git commits behind:"
+    git fetch
+    git rev-list --count HEAD..@{u}
+# TDL: save commits to varialbe and then display the following if greater than 1
+    echo "\"./ANE.sh --update\" or \"git pull\" to update"
     exit
 fi
 
@@ -39,20 +51,31 @@ if [ "$1" = "--settings" ]; then
     exit
 fi
 
+# update ANE with a git pull
+if [ "$1" = "--update" ]; then
+    git pull
+    exit
+fi
+
 # update.sh help menu
 if [ "$1" = "--help" ]; then
+    echo "ANE HELP:"
     echo "--help"
-    echo "  This menu"
+    echo "  This menu!"
     echo "--enabled"
     echo "  List enabled apps"
-    echo "--git"
-    echo "  Check # of git commits you are behind"
+    echo "--install"
+    echo "  Install ANE \(first time or reset\)"
+    echo "--outdated, --updates"
+    echo "  Check # of git commits your install of ANE is behind"
     echo "--prune"
-    echo "  Prune docker unused images and volumes"
+    echo "  Prune unused Docker images and volumes"
     echo "--requirements"
-    echo "  Install ANE requirements"
+    echo "  Install/reinstall ANE requirements"
     echo "--settings"
-    echo "  Edit ANE settings"
+    echo "  Edit ANE settings/overrides"
+    echo "--update"
+    echo "  Update ANE files from git"
     exit
 fi
 
