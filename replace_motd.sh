66412a6e62461d5aa7ba6381a2180797496fa25c
Bill Curran <bcurra3@yahoo.com>
2025-07-17 15:21:16 -0700

    delete some old files

--- replace_motd.sh
+++
@@ -1,2 +0,0 @@
-figlet $(hostname) > roles/ansible-nas-general/files/motd.txt
-echo Ansible-NAS Extended >> roles/ansible-nas-general/files/motd.txt
--- settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == "--enabled" ]]; then
-cat inventories/supernas/group_vars/nas.yml |grep 'enabled: true'
-exit
-fi
-
-nano inventories/supernas/group_vars/nas.yml
