9d71ac0a2583424f44d04adf607dcdeb2b318690
Bill Curran <bcurra3@yahoo.com>
2025-07-18 18:46:49 -0700

    ane.sh feature complete, install_ane.sh started

--- ane.sh
+++ ane.sh
@@ -1,118 +1,171 @@
 #!/bin/bash
 ##
-##      _         _   _       _____
-##     / \       | \ | |     | ____|
-##    / _ \ _____|  \| |_____|  _|
-##   / ___ \_____| |\  |_____| |___
-##  /_/   \_\    |_| \_|     |_____|
+##      _    _   _ _____
+##     / \  | \ | | ____|
+##    / _ \ |  \| |  _|
+##   / ___ \| |\  | |___
+##  /_/   \_\_| \_|_____|
 ##
 ##  Ansible-NAS-Enhanced - https://github.com/bcurran3/ansible-nas-enhanced
 
-if [ "$1" = "--enabled" ]; then
-     echo "*** ANE enabled apps:"
+function help {
+# ANE help menu
+    echo "Ansible-NAS-Enhanced (ANE) Help:"
+    echo "  --help"
+    echo "    What you see now!"
+############ should probably merge this to just one switch
+    echo "  --app <app_name>"
+    echo "    Install/Update one app by tag."
+    echo "  --apps <app_name> <app_name> <app_name> <app_name>"
+    echo "    Install/Update multiple apps by tag."
+############
+    echo "  --enabled"
+    echo "    List ANE enabled apps."
+    echo "  --install"
+    echo "    Install ANE (first time or reset)."
+    echo "  --inventory"
+    echo "    Edit ANE inventory file."
+    echo "  --outdated, --updates"
+    echo "    Check # of git commits your install of ANE is behind."
+    echo "  --permissions"
+    echo "    Reset permissions on all shared files."
+    echo "  --prune"
+    echo "    Prune unused Docker images and volumes."
+    echo "  --requirements"
+    echo "    Install/Re-install ANE requirements."
+    echo "  --run"
+    echo "    Run ANE full playbook."
+    echo "  --settings"
+    echo "    Edit ANE settings/overrides."
+    echo "  --update"
+    echo "    Update ANE files from git"
+    exit
+}
+
+if [[ -z "$1" ]]; then
+       echo "  ** Run \"./ane.sh --help\" for help :-)"
+     exit
+fi
+
+if [[ "$1" = "--ap" || "$1" = "-ap" || "$1" = "--aps" || "$1" = "-aps" ]]; then
+     echo "  ** You typoed! :-("
+     exit
+fi
+
+if [[ "$1" = "--app" || "$1" = "-app" ]]; then
+     if [ "$2" = '' ]; then
+        echo "  ** You need to specify an app name/tag."
+        exit
+     fi
+     ansible-playbook -i inventories/ANE/inventory nas.yml -b -K -t $2
+     exit
+fi
+
+if [[ "$1" = "--apps" || "$1" = "-apps" ]]; then
+     if [ "$2" = '' ]; then
+        echo "  ** You need to specify an app name/tag."
+        exit
+     fi
+   appslist=""
+   for arg in "$@"; do
+     if [[ "$1" = "--apps" || "$1" = "-apps" ]]; then
+       continue
+     fi
+       appslist+="-t $arg "
+   done
+   appslist=$(echo "$appslist" | sed 's/ $//')
+   ansible-playbook -i inventories/ANE/inventory nas.yml -b -K $appslist
+   exit
+fi
+
+if [[ "$1" = "--enabled" || "$1" = "-enabled" ]]; then
+     echo "ANE enabled apps:"
      cat inventories/ANE/group_vars/nas.yml |grep 'enabled: true'
      exit
 fi
 
 # git force pull
-# DO NOT USE: for dev use
-if [ "$1" = "--gitforcepull" ]; then
+# DO NOT USE! - for dev use only
+if [[ "$1" = "--gitforcepull" || "$1" = "-gitforcepull" ]]; then
     git fetch --all
     git reset --hard origin/main
     exit
 fi
 
-# install ANE
-if [ "$1" = "--install" ]; then
+if [[ "$1" = "--help" || "$1" = "-help" ]]; then
+   help
+fi
+# Install ANE
+if [[ "$1" = "--install" || "$1" = "-install" ]]; then
     if [ -d "inventories/ANE" ]; then
-       echo "WARNING: inventories/ANE exists!"
-       echo "         \"rm inventories/ANE -R\" first if you really wish to reset."
+       echo "  ** WARNING: inventories/ANE exists!"
+       echo "  ** \"rm inventories/ANE -R\" first if you really wish to reset."
        exit
     fi
     cp -rfp inventories/sample inventories/ANE
-    echo "Time to configure!"
-    echo "\"./ane.sh --inventory\" to edit your inventory file"
-    echo "\"./ane.sh --settings\" to edit your settings file"
+    echo "  ** Time to configure!"
+    echo "  ** \"./ane.sh --inventory\" to edit your inventory file"
+    echo "  ** \"./ane.sh --settings\" to edit your settings/overrides file"
     exit
 fi
 
-# edit inventory file
-if [ "$1" = "--inventory" ]; then
+# Edit inventory file
+if [[ "$1" = "--inventory" || "$1" = "-inventory" ]]; then
     nano inventories/ANE/inventory
     exit
 fi
 
 # git check commits
-if [ "$1" = "--outdated" ]; then
+if [[ "$1" = "--outdated" || "$1" = "-outdated" || "$1" = "--updates" || "$1" = "-updates" ]]; then
 #TDL: add --updates
     git fetch --quiet
     BEHIND=$(git rev-list --count HEAD..@{u})
-    echo "Your ANE installation is $BEHIND git commits behind."
+    echo "  ** Your ANE installation is $BEHIND git commits behind."
     if [ $BEHIND -gt 1 ]; then
-       echo "\"./ane.sh --update\" or \"git pull\" to update"
+       echo "  ** \"./ane.sh --update\" or \"git pull\" to update"
     fi
     exit
 fi
 
 # reset shared file permissions
-if [ "$1" = "--permissions" ]; then
+if [[ "$1" = "permissions" || "$1" = "-permissions" ]]; then
     ansible-playbook -i inventories/ANE/inventory permission_data.yml -b -K
     exit
 fi
 
 # prune docker stuff to save space
-if [ "$1" = "--prune" ]; then
-    echo y | docker image prune
-    echo y | docker volume prune
+if [[ "$1" = "--prune" || "$1" = "-prune" ]]; then
+    echo "  ** Pruning images..."
+    docker image prune -f
+    echo "  ** Pruning volumes..."
+    docker volume prune -f
     exit
 fi
 
 # install ANE requirements
-if [ "$1" = "--requirements" ]; then
+if [[ "$1" = "--requirements" || "$1" = "-requirements" ]]; then
     ansible-galaxy install -r requirements.yml
     exit
 fi
 
+# Run ANE full playbook
+if [[ "$1" = "--run" || "$1" = "-run" ]]; then
+    ansible-playbook -i inventories/ANE/inventory nas.yml -b -K $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+    exit
+fi
+
 # edit ANE settings/variables
-if [ "$1" = "--settings" ]; then
+if [[ "$1" = "--settings" || "$1" = "-settings" ]]; then
     nano inventories/ANE/group_vars/nas.yml
     exit
 fi
 
 # update ANE files
-if [ "$1" = "--update" ]; then
+if [[ "$1" = "--update" || "$1" = "-update" ]]; then
     git pull
     exit
 fi
 
-# ANE help menu
-if [ "$1" = "--help" ]; then
-    echo "Ansible-NAS-Enhanced (ANE) Help:"
-    echo "  --help"
-    echo "    This menu!"
-    echo "  --enabled"
-    echo "    List ANE enabled apps"
-    echo "  --install"
-    echo "    Install Anisible-NAS-Enhanced (first time or reset)"
-    echo "  --inventory"
-    echo "    Edit ANE inventory file"
-    echo "  --outdated, --updates"
-    echo "    Check # of git commits your install of ANE is behind"
-    echo "  --permissions"
-    echo "    Reset permissions on all shared files"
-    echo "  --prune"
-    echo "    Prune unused Docker images and volumes"
-    echo "  --requirements"
-    echo "    Install/re-install ANE requirements"
-    echo "  --settings"
-    echo "    Edit ANE settings/overrides"
-    echo "  -t appname"
-###         ^^^^ going to make this more intuitive
-    echo "    Update ANE app"
-    echo "  --update"
-    echo "    Update ANE files from git"
-    exit
-fi
-
-# --tags "taskname"
-ansible-playbook -i inventories/ANE/inventory nas.yml -b -K $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+# show help for all bad arguments
+echo "  ** Ansible-NAS-Enhanced (ANE) unrecognized switch."
+echo "  ** Run \"./ane.sh --help\" for help :-)"
---
+++ scripts/install_ane.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+##
+##      _    _   _ _____
+##     / \  | \ | | ____|
+##    / _ \ |  \| |  _|
+##   / ___ \| |\  | |___
+##  /_/   \_\_| \_|_____|
+##
+##  Ansible-NAS-Enhanced - https://github.com/bcurran3/ansible-nas-enhanced
+
+### VARS
+REPO="https://gitea.mrbillsabode.org/bill/supernas"
+INSTALL_DIR="ANE"
+
+### FUNCTIONS
+
+# print intention
+function print1 {
+  echo -e "\033[36m ** INFO: $1\033[0m"
+}
+
+# print success
+function print2 {
+  echo -e "\033[32m ** SUCCESS: $1\033[0m"
+}
+
+# print error
+function print3 {
+  echo -e "\033[31m ** ERROR: $1\033[0m"
+}
+
+function print_info {
+    echo -e " **** \033[01mAnsible-NAS-Enhanced (ANE) installation script. ****\033[0m"
+    print1 "This script will make the directory $(pwd)/$INSTALL_DIR"
+    print1 "and clone the files from $REPO into it."
+    print1 "apt will be used to install required packages (Ansible, nano, etc.)."
+    print1 "That pretty much covers it. Enjoy!"
+    while true; do
+        read -p "Do you want to continue (y/n)? " -n 1 -r yn
+        case "$yn" in
+            [Yy]* ) printf "\n"; return 0;;
+            [Nn]* ) printf "\n"; print3 "Aborting..."; exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+function get_os {
+    if [ -f /etc/lsb-release ]; then
+       print1 "Getting OS and version..."
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+        VER=$DISTRIB_RELEASE
+        print2 "Found $OS $VER"
+    fi
+}
+
+function install_git {
+    print1 "Updating apt cache..."
+    sudo apt update
+    print1 "Installing git..."
+    sudo apt install git -y
+    print2 "git installed."
+}
+
+function clone_repo {
+    if [ -d ./ansible-nas-enhanced ]; then
+       print3 "Please remove ./ansible-nas-enhanced and try again."
+       exit
+    fi
+    print1 "Making directory for git files..."
+    mkdir ./ansible-nas-enhanced
+    cd ./ansible-nas-enhanced
+    print1 "Cloning ANE repo..."
+    git clone $REPO
+    print2 "ANE repo cloned."
+}
+
+function install_packages {
+    print1 "Updating apt cache..."
+    sudo apt update
+    print1 "Installing required packages (Ansible and nano)..."
+    if [[ "$VER" = "22.04" ]]; then
+# ansible requires adding the repo to get a newer version
+        sudo apt install git nano -y
+    fi
+    if [[ "$VER" = "24.04" ]]; then
+        sudo apt install ansible nano -y
+    fi
+    if [[ "$VER" = "25.04" ]]; then
+        sudo apt install ansible nano -y
+    fi
+    print2 "Required packages installed."
+}
+
+print_info
+get_os
+install_git
+clone_repo
+install_packages
