761747b6862dbb9c30e692b833de277ea5e87ac9
Bill Curran <bcurra3@yahoo.com>
2025-12-11 13:54:19 -0800

    readability tweaks

--- scripts/install_ane.sh
+++ scripts/install_ane.sh
@@ -17,34 +17,41 @@ INSTALL_DIR="ANE"
 
 ### FUNCTIONS
 
+# check if run with sudo
+function check_sudo {
+  if ! [ $(id -u) = 0 ]; then
+    print_info "You MAY need to use sudo."
+  fi
+}
+
 # print intention
-function print1 {
+function print_info {
   echo -e "\033[36m ** INFO: $1\033[0m"
 }
 
 # print success
-function print2 {
+function print_success {
   echo -e "\033[32m ** SUCCESS: $1\033[0m"
 }
 
 # print error
-function print3 {
+function print_error {
   echo -e "\033[31m ** ERROR: $1\033[0m"
   exit 1
 }
 
 function print_info {
     echo -e " **** \033[01mAnsible-NAS-Enhanced (ANE) installation script. ****\033[0m"
-    print1 "This script will:"
-    print1 "  - Upgrade apt packages and install required packages (Ansible, git, nano, etc.)."
-    print1 "  - Clone the ANE files from $REPO to $(pwd)/$INSTALL_DIR."
-    print1 "  - Setup ANE for first time use."
-    print1 "  - That pretty much covers it. Enjoy!"
+    print_info "This script will:"
+    print_info "  - Upgrade apt packages and install required packages (Ansible, git, nano, etc.)."
+    print_info "  - Clone the ANE files from $REPO to $(pwd)/$INSTALL_DIR."
+    print_info "  - Setup ANE for first time use."
+    print_info "  - That pretty much covers it. Enjoy!"
     while true; do
         read -p "Do you want to continue (y/n)? " -n 1 -r yn
         case "$yn" in
             [Yy]* ) printf "\n"; return 0;;
-            [Nn]* ) printf "\n"; print3 "Aborting...";;
+            [Nn]* ) printf "\n"; print_error "Aborting...";;
             * ) echo "Please answer yes or no.";;
         esac
     done
@@ -52,70 +59,70 @@ function print_info {
 
 function get_os {
     if [ -f /etc/lsb-release ]; then
-       print1 "Getting OS and version..."
+       print_info "Getting OS and version..."
         . /etc/lsb-release
-        if [ $? -ne 0 ]; then print3 "Unknown distribution. Aborting..."; fi
+        if [ $? -ne 0 ]; then print_error "Unknown distribution. Aborting..."; fi
         OS=$DISTRIB_ID
         VER=$DISTRIB_RELEASE
-        print2 "Found $OS $VER"
+        print_success "Found $OS $VER"
     fi
 }
 
 function install_git {
-    print1 "Updating apt cache and upgrading packages..."
+    print_info "Updating apt cache and upgrading packages..."
     sudo apt update && sudo apt upgrade -y
-    if [ $? -ne 0 ]; then print3 "Aborting..."; fi
-    print2 "apt packages upgraded."
-    print1 "Installing git..."
+    if [ $? -ne 0 ]; then print_error "Aborting..."; fi
+    print_success "apt packages upgraded."
+    print_info "Installing git..."
     sudo apt install git -y
-    if [ $? -ne 0 ]; then print3 "Aborting..."; fi
-    print2 "git installed."
+    if [ $? -ne 0 ]; then print_error "Aborting..."; fi
+    print_success "git installed."
 }
 
 function clone_repo {
-    print1 "Cloning ANE repo..."
+    print_info "Cloning ANE repo..."
     git clone $REPO $INSTALL_DIR
-    if [ $? -ne 0 ]; then print3 "Aborting..."; fi
-    print2 "ANE repo cloned to $(pwd)/$INSTALL_DIR."
+    if [ $? -ne 0 ]; then print_error "Aborting..."; fi
+    print_success "ANE repo cloned to $(pwd)/$INSTALL_DIR."
 }
 
 function install_packages {
-    print1 "Installing ANE required packages (Ansible and nano)..."
+    print_info "Installing ANE required packages (Ansible and nano)..."
     if [[ "$VER" = "22.04" ]]; then
         sudo apt-add-repository ppa:ansible/ansible -y
-        if [ $? -ne 0 ]; then print3 "Aborting..."; fi
+        if [ $? -ne 0 ]; then print_error "Aborting..."; fi
     fi
     if [[ "$VER" = "24.04" ]]; then
         sudo apt-add-repository ppa:ansible/ansible -y
-        if [ $? -ne 0 ]; then print3 "Aborting..."; fi
+        if [ $? -ne 0 ]; then print_error "Aborting..."; fi
     fi
         sudo apt update && sudo apt install ansible nano -y
-        if [ $? -ne 0 ]; then print3 "Aborting..."; fi
-    print2 "Required packages installed."
+        if [ $? -ne 0 ]; then print_error "Aborting..."; fi
+    print_success "Required packages installed."
 }
 
 function default_config {
     cd $INSTALL_DIR
-    print1 "Copying ANE default config files..."
+    print_info "Copying ANE default config files..."
     cp -rfp inventories/sample inventories/ANE
-    if [ $? -ne 0 ]; then print3 "Aborting..."; fi
-    print2 "ANE default config files copied."
-    print1 "Installing ANE required Ansible Galaxy roles..."
+    if [ $? -ne 0 ]; then print_error "Aborting..."; fi
+    print_success "ANE default config files copied."
+    print_info "Installing ANE required Ansible Galaxy roles..."
     ansible-galaxy install -r requirements.yml
-    if [ $? -ne 0 ]; then print3 "Aborting..."; fi
-    print2 "ANE required Ansible Galaxy roles installed."
-    print1 "Ensuring ane.sh is executable."
+    if [ $? -ne 0 ]; then print_error "Aborting..."; fi
+    print_success "ANE required Ansible Galaxy roles installed."
+    print_info "Ensuring ane.sh is executable."
     sudo chmod +x ./ane.sh
-    if [ $? -ne 0 ]; then print3 "Aborting..."; fi
-    print2 "ane.sh is executable."
+    if [ $? -ne 0 ]; then print_error "Aborting..."; fi
+    print_success "ane.sh is executable."
 }
 
 function setup_instructions {
     echo -e " **** \033[01mAnsible-NAS-Enhanced (ANE) installed! ****\033[0m"
-    print1 "\"./ane.sh --inventory\" to edit your inventory file"
-    print1 "\"./ane.sh --settings\" to edit your settings/overrides file (enable apps)"
-    print1 "\"./ane.sh --run\" to run the ANE playbook"
-    print1 "\"./ane.sh --help\" for more options"
+    print_info "\"./ane.sh --inventory\" to edit your inventory file"
+    print_info "\"./ane.sh --settings\" to edit your settings/overrides file (enable apps)"
+    print_info "\"./ane.sh --run\" to run the ANE playbook"
+    print_info "\"./ane.sh --help\" for more options"
 }
 
 ### INSTALL
@@ -125,14 +132,15 @@ if [ "$1" ]; then
 fi
 
 if [ -d ./$INSTALL_DIR ]; then
-    print3 "Please remove or rename ./$INSTALL_DIR and try again."
+    print_error "Please remove or rename ./$INSTALL_DIR and try again."
 fi
 
+check_sudo
 print_info
 get_os
 install_git
 clone_repo
 install_packages
 default_config
-cd ANE
+cd $INSTALL_DIR
 setup_instructions
